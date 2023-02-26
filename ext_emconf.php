e28de4a81b4711b5801c42e44a81f3524fb86e41
Frank Berger <fberger@sudhaus7.de>
2023-02-26 15:25:16 +0100

    implement basic functions

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,10 +13,10 @@
 
 $EM_CONF['siteconfiggitsync'] = [
     'title' => '(Code711) Site Config Git Sync',
-    'description' => 'This extension will push changes to the site config yaml files back to your gitlab repository by creating a branch for the changes along with a merge-request. It is targeted towards automated CI/CD installations so no local git binary or .git directory is needed.',
+    'description' => 'This extension will push changes to the site config yaml files back to your gitlab repository by creating a branch for the changes along with a merge-request. It is targeted towards automated CI/CD installations or sites in general where the site-config is git versioned. No local git binary or .git directory is needed.',
     'category' => 'plugin',
-    'version' => '1.0.0',
-    'state' => 'stable',
+    'version' => '0.9.0',
+    'state' => 'beta',
     'clearcacheonload' => 1,
     'author' => 'Frank Berger',
     'author_email' => 'fberger@code711.de',
