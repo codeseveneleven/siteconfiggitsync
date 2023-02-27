df509ad149c780966e878153a8314bb970c5bf41
Frank Berger <fberger@sudhaus7.de>
2023-02-27 11:22:51 +0100

    Only run in TYPO3_CONTEXT=Production

--- Classes/Configuration/SiteConfiguration.php
+++ Classes/Configuration/SiteConfiguration.php
@@ -33,76 +33,82 @@ class SiteConfiguration extends \TYPO3\CMS\Core\Configuration\SiteConfiguration
     public function write(string $siteIdentifier, array $configuration, bool $protectPlaceholders = false): void
     {
         parent::write($siteIdentifier, $configuration, $protectPlaceholders);
-        try {
-            $folder   = $this->configPath . '/' . $siteIdentifier;
-            $fileName = $folder . '/' . $this->configFileName;
+        if (Environment::getContext()->isProduction()) {
+            try {
+                $folder   = $this->configPath . '/' . $siteIdentifier;
+                $fileName = $folder . '/' . $this->configFileName;
 
-            $git     = GitApiServiceFactory::get();
-            $branch  = $git->getBranchName($siteIdentifier);
-            $message = $git->getCommitMessage($siteIdentifier, 'create or update');
-            if ($git->createBranch($branch)) {
-                $filebase = \str_replace(Environment::getProjectPath(), '', $fileName);
-                if ($git->commitFile($filebase, (string)\file_get_contents($fileName), $message, $branch)) {
-                    $git->createMergeRequest($siteIdentifier, $branch, 'create or update');
+                $git     = GitApiServiceFactory::get();
+                $branch  = $git->getBranchName($siteIdentifier);
+                $message = $git->getCommitMessage($siteIdentifier, 'create or update');
+                if ($git->createBranch($branch)) {
+                    $filebase = \str_replace(Environment::getProjectPath(), '', $fileName);
+                    if ($git->commitFile($filebase, (string)\file_get_contents($fileName), $message, $branch)) {
+                        $git->createMergeRequest($siteIdentifier, $branch, 'create or update');
+                    }
                 }
-            }
-        } catch (\InvalidArgumentException $e) {
-            $message = GeneralUtility::makeInstance(
-                FlashMessage::class,
-                $e->getMessage(),
-                '',
-                FlashMessage::WARNING,
-                true
-            );
+            } catch (\InvalidArgumentException $e) {
+                $message = GeneralUtility::makeInstance(
+                    FlashMessage::class,
+                    $e->getMessage(),
+                    '',
+                    FlashMessage::WARNING,
+                    true
+                );
 
-            $flashMessageService = GeneralUtility::makeInstance(FlashMessageService::class);
-            $messageQueue = $flashMessageService->getMessageQueueByIdentifier();
-            $messageQueue->addMessage($message);
+                $flashMessageService = GeneralUtility::makeInstance(FlashMessageService::class);
+                $messageQueue        = $flashMessageService->getMessageQueueByIdentifier();
+                $messageQueue->addMessage($message);
+            }
         }
     }
 
     public function rename(string $currentIdentifier, string $newIdentifier): void
     {
         parent::rename($currentIdentifier, $newIdentifier);
-        try {
-            $folderOld = $this->configPath . '/' . $currentIdentifier;
-            $fileNameOld = $folderOld . '/' . $this->configFileName;
-            $folderNew = $this->configPath . '/' . $newIdentifier;
-            $fileNameNew = $folderNew . '/' . $this->configFileName;
+        if (Environment::getContext()->isProduction()) {
+            try {
+                $folderOld   = $this->configPath . '/' . $currentIdentifier;
+                $fileNameOld = $folderOld . '/' . $this->configFileName;
+                $folderNew   = $this->configPath . '/' . $newIdentifier;
+                $fileNameNew = $folderNew . '/' . $this->configFileName;
 
-            $git = GitApiServiceFactory::get();
-            $branch = $git->getBranchName($newIdentifier);
-            $message = $git->getCommitMessage($newIdentifier, 'rename site identifier');
+                $git     = GitApiServiceFactory::get();
+                $branch  = $git->getBranchName($newIdentifier);
+                $message = $git->getCommitMessage($newIdentifier, 'rename site identifier');
 
-            if ($git->createBranch($branch)) {
-                $filebaseOld = \str_replace(Environment::getProjectPath(), '', $fileNameOld);
-                $filebaseNew = \str_replace(Environment::getProjectPath(), '', $fileNameNew);
+                if ($git->createBranch($branch)) {
+                    $filebaseOld = \str_replace(Environment::getProjectPath(), '', $fileNameOld);
+                    $filebaseNew = \str_replace(Environment::getProjectPath(), '', $fileNameNew);
 
-                if ($git->moveFile($filebaseOld, $filebaseNew, $message, $branch)) {
-                    $git->createMergeRequest($newIdentifier, $branch, 'rename site identifier');
+                    if ($git->moveFile($filebaseOld, $filebaseNew, $message, $branch)) {
+                        $git->createMergeRequest($newIdentifier, $branch, 'rename site identifier');
+                    }
                 }
+            } catch (\InvalidArgumentException $e) {
             }
-        } catch (\InvalidArgumentException $e) {
         }
     }
 
     public function delete(string $siteIdentifier): void
     {
         parent::delete($siteIdentifier);
-        try {
-            $folder   = $this->configPath . '/' . $siteIdentifier;
-            $fileName = $folder . '/' . $this->configFileName;
+        if (Environment::getContext()->isProduction()) {
+            try {
+                $folder   = $this->configPath . '/' . $siteIdentifier;
+                $fileName = $folder . '/' . $this->configFileName;
 
-            $git     = GitApiServiceFactory::get();
-            $branch  = $git->getBranchName($siteIdentifier);
-            $message = $git->getCommitMessage($siteIdentifier, 'delete site');
-            if ($git->createBranch($branch)) {
-                $filebase = \str_replace(Environment::getProjectPath(), '', $fileName);
-                if ($git->deleteFile($filebase, $branch, $message)) {
-                    $git->createMergeRequest($siteIdentifier, $branch, 'delete site');
+                $git     = GitApiServiceFactory::get();
+                $branch  = $git->getBranchName($siteIdentifier);
+                $message = $git->getCommitMessage($siteIdentifier, 'delete site');
+                if ($git->createBranch($branch)) {
+                    $filebase = \str_replace(Environment::getProjectPath(), '', $fileName);
+                    if ($git->deleteFile($filebase, $branch, $message)) {
+                        $git->createMergeRequest($siteIdentifier, $branch, 'delete site');
+                    }
                 }
+            } catch (\InvalidArgumentException $e) {
             }
-        } catch (\InvalidArgumentException $e) {
         }
     }
 }
